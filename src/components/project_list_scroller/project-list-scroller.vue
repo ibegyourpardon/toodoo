f19e1971fc1e6642c1de8e536500d46f6d272c54
hash rail <yiziyint@gmail.com>
2022-07-16 14:59:20 +0800

    aha ,it's a circle

--- src/components/project_list_scroller/project-list-scroller.vue
+++ src/components/project_list_scroller/project-list-scroller.vue
@@ -25,6 +25,20 @@
         </a>
       </li>
 
+      <li class="mb-2 px-4 py-4 text-gray-100 flex flex-row  border-gray-300
+              hover:text-black   hover:bg-gray-300  hover:font-bold rounded-lg">
+        <span>
+          <svg class="fill-current h-5 w-5 " viewBox="0 0 24 24">
+            <path d="M16 20h4v-4h-4m0-2h4v-4h-4m-6-2h4V4h-4m6
+                        4h4V4h-4m-6 10h4v-4h-4m-6 4h4v-4H4m0 10h4v-4H4m6
+                        4h4v-4h-4M4 8h4V4H4v4z"></path>
+          </svg>
+        </span>
+        <a href="#">
+          <span class="ml-2">Today</span>
+        </a>
+      </li>
+
     </ul>
 
   </div>
